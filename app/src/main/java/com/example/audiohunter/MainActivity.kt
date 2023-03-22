ec0eb114054a851c29fa588e3c3d4824421a12ee
JORGE MIGUEL TEIXEIRA DO NASCIMENTO LISBOA <jorge.mtnl@puccampinas.edu.br>
2023-03-21 22:27:00 -0300

    projeto

---
+++ app/src/main/java/com/example/audiohunter/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.audiohunter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var screen: ActivityMainBinding // Cria uma variável da nossa tela
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        screen = ActivityMainBinding.inflate(layoutInflater) // Transforma XML em KT
+        setContentView(screen.root) // Coloca na tela
+
+
+        screen.tvSoundPressureLevel.text = "67.58 dB"
+    }
+}
