978a491709cbe1a8cdda86cf64459556a8bcd20d
rafinhas1901 <rafael-1901@hotmail.com>
2023-04-08 22:07:24 -0300

    ajustes finais de troca de cor apos 72.5 dB

--- app/src/main/java/com/example/audiohunter/MainActivity.kt
+++ app/src/main/java/com/example/audiohunter/MainActivity.kt
@@ -54,9 +54,17 @@ class MainActivity : AppCompatActivity() {
             Thread {
                 while (isRunning) {
                     runOnUiThread {
+                        if (audioCaptureService.db > 72.5)
+                        {
+                            screen.tvSoundPressureLevel.setTextColor(resources.getColor(R.color.puc_yellow))
+                        } else {
+                            screen.tvSoundPressureLevel.setTextColor(resources.getColor(R.color.white))
+                        }
+                        Log.d("AH_SOUND","Valor mic: ${audioCaptureService.db}")
+                        Log.d("AH_SOUND","----")
                         screen.tvSoundPressureLevel.text = "${String.format("%.2f", audioCaptureService.db)} dB"
                     }
-                    Thread.sleep(1000) // Espera 1 segundo antes de verificar novamente
+                    Thread.sleep(2000) // Espera 1 segundo antes de verificar novamente
                 }
             }.start()
 
@@ -65,82 +73,5 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         isRunning = false
-    }
-//    override fun onRestart() {
-//        super.onRestart()
-//        val decibel = calculateSPL().roundToInt().toString()
-//        Log.d("ON_RESTART", decibel)
-//        screen.tvSoundPressureLevel.text = "${String.format("%.2f", calculateSPL())} dB"
-//    }
-//
-//    override fun onResume() {
-//        super.onResume()
-//        val decibel = calculateSPL().roundToInt().toString()
-//        Log.d("ON_RESUME", decibel)
-//        screen.tvSoundPressureLevel.text = "${String.format("%.2f", calculateSPL())} dB"
-//    }
-    private fun calculateSPL(): Double {
-        // Configura a gravação de áudio
-
-        val audioBufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT)
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            requestRecordAudioPermission()
-        }
-        val audioRecord = AudioRecord(MIC, SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT, audioBufferSize)
-        audioRecord.startRecording()
-
-
-        // Captura 1 segundo de áudio
-        val audioSamples = ShortArray(SAMPLE_RATE)
-        var readSize = 0
-        while (readSize < SAMPLE_RATE) {
-            readSize += audioRecord.read(audioSamples, readSize, SAMPLE_RATE - readSize)
-        }
-
-        // Calcula o nível de pressão sonora médio (SPL)
-        var rms = 0.0
-        for (sample in audioSamples) {
-            rms += sample * sample.toDouble()
-        }
-        rms = Math.sqrt(rms / audioSamples.size)
-        val db = 20 * log10(rms / referencia) - 94
-
-        // Libera recursos de gravação de áudio
-        audioRecord.stop()
-        audioRecord.release()
-
-        // Retorna o valor do SPL em decibéis (dB)
-        return db
-    }
-
-    private fun AppCompatActivity.requestRecordAudioPermission() {
-        try {
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.RECORD_AUDIO
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.RECORD_AUDIO),
-                    RECORD_AUDIO_PERMISSION_REQUEST_CODE
-                )
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            // Tratar exceção aqui, se necessário
-        }
-    }
-    // Método para manipular a resposta do usuário às solicitações de permissão
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (requestCode == RECORD_AUDIO_PERMISSION_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // A permissão foi concedida. Você pode iniciar a gravação de áudio aqui.
-            } else {
-                // A permissão foi negada. Você pode exibir uma mensagem para o usuário aqui.
-            }
-        }
     }
 }
