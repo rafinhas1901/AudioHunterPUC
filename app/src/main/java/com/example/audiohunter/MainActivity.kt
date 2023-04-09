d4cbc5ecec63ea6c9039e0609eb80aa59813d14d
jorge lisboa <jorgelisboa659@gmail.com>
2023-04-09 16:14:40 -0300

    Notificação / vibração / som de notificação / não buga quando rejeita permissão

--- app/src/main/java/com/example/audiohunter/MainActivity.kt
+++ app/src/main/java/com/example/audiohunter/MainActivity.kt
@@ -2,30 +2,31 @@ package com.example.audiohunter
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.media.AudioRecord
-import android.media.MediaRecorder.AudioSource.MIC
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.audiohunter.databinding.ActivityMainBinding
 import com.example.audiohunter.utils.AudioCaptureService
-import com.example.audiohunter.utils.AudioHunterService
-import java.lang.Math.log10
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.coroutineContext
-import kotlin.math.roundToInt
 
+//"val" é constante, "var" é variavel
+    const val CHANNEL_ID = "channelID"
+    const val CHANNEL_NAME = "channelName"
+    const val NOTIFICATION_ID = 0
 class MainActivity : AppCompatActivity() {
     private lateinit var screen: ActivityMainBinding// Cria uma variável da nossa tela
     private val RECORD_AUDIO_PERMISSION_REQUEST_CODE = 1
-    private val referencia = 2e-5
-    private val SAMPLE_RATE = 44100
-    private val CHANNEL_CONFIG = android.media.AudioFormat.CHANNEL_IN_MONO
-    private val AUDIO_FORMAT = android.media.AudioFormat.ENCODING_PCM_16BIT
     private var isRunning = true
 
     @SuppressLint("SetTextI18n")
@@ -43,9 +44,9 @@ class MainActivity : AppCompatActivity() {
             //    ActivityCompat#requestPermissions
             ActivityCompat.requestPermissions(this,
                 arrayOf(Manifest.permission.RECORD_AUDIO),
-                1)
+                RECORD_AUDIO_PERMISSION_REQUEST_CODE)
+            return
         }
-
             // Inicia o AudioCaptureThread
             val audioCaptureService = AudioCaptureService()
             audioCaptureService.start()
@@ -53,8 +54,19 @@ class MainActivity : AppCompatActivity() {
             Thread {
                 while (isRunning) {
                     runOnUiThread {
-                        if (audioCaptureService.db > 72.5)
+                        if (audioCaptureService.db > 10.5)
                         {
+                            createNotification()
+                            val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+                                .setContentTitle("Ambiente com muito barulho - ${String.format("%.2f", audioCaptureService.db)} dB")
+                                .setContentText("Este ambiente ser prejudicial a saúde.")
+                                .setSmallIcon(R.drawable.enabled_mic)
+                                .setPriority(NotificationManager.IMPORTANCE_MAX)
+                                .setVibrate(longArrayOf(1000, 1000))
+                                .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+                                .build()
+                            val notificationManager = NotificationManagerCompat.from(this)
+                            notificationManager.notify(NOTIFICATION_ID, notification)
                             screen.tvSoundPressureLevel.setTextColor(resources.getColor(R.color.puc_yellow))
                         } else {
                             screen.tvSoundPressureLevel.setTextColor(resources.getColor(R.color.white))
@@ -66,6 +78,33 @@ class MainActivity : AppCompatActivity() {
                     Thread.sleep(2000) // Espera 1 segundo antes de verificar novamente
                 }
             }.start()
+    }
+
+    //Métodos são "fun"
+    private fun createNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH).apply {
+                lightColor = Color.GREEN
+                enableLights(true)
+                enableVibration(true)
+            }
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(grantResults[0] == 0) { //0 deu bom, -1 não aceitou
+            finish()
+            startActivity(intent)
+        } else {
+            screen.tvSoundPressureLevel.textSize = 20F
+            screen.tvSoundPressureLevel.text = "Libere a permissão de microfone nas configurações"
+        }
 
     }
 
