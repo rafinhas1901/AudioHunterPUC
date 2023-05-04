6a43fd5e511cd4008dcd3f8508d61109496e53e4
rafinhas1901 <rafael-1901@hotmail.com>
2023-05-03 22:17:13 -0300

    envia panico para o firebase

--- app/src/main/java/com/example/audiohunter/MainActivity.kt
+++ app/src/main/java/com/example/audiohunter/MainActivity.kt
@@ -4,10 +4,8 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.ContentValues.TAG
 import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.location.Location
@@ -15,7 +13,6 @@ import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
@@ -25,69 +22,38 @@ import com.example.audiohunter.utils.AudioCaptureService
 import com.google.android.gms.location.LocationServices
 import com.google.firebase.firestore.FirebaseFirestore
 
-//"val" é constante, "var" é variavel
-    const val CHANNEL_ID = "channelID"
-    const val CHANNEL_NAME = "channelName"
-    const val NOTIFICATION_ID = 0
+// Variáveis para notificações
+const val CHANNEL_ID = "channelID"
+const val CHANNEL_NAME = "channelName"
+const val NOTIFICATION_ID = 0
 class MainActivity : AppCompatActivity() {
-    private lateinit var screen: ActivityMainBinding// Cria uma variável da nossa tela
-    private val RECORD_AUDIO_PERMISSION_REQUEST_CODE = 1
+    //private var valoresDb = arrayOf("")
+    private lateinit var screen: ActivityMainBinding // Cria uma variável da nossa tela
     private var isRunning = true
-
+    // Lista de permissões a serem solicitadas
+    private val PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.RECORD_AUDIO
+    )
+    private val audioCaptureService = AudioCaptureService()
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         screen = ActivityMainBinding.inflate(layoutInflater) // Transforma XML em KT
         setContentView(screen.root) // Coloca na tela
-        fun sendLocation() {
-            // Obter a localização atual do dispositivo
-            val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-            fusedLocationClient.lastLocation
-                .addOnSuccessListener { location: Location? ->
-                    if (location != null) {
-                        // Enviar a localização para o Firestore
-                        val db = FirebaseFirestore.getInstance()
-                        val docRef = db.collection("warnings").document()
-                        val locationData = hashMapOf(
-                            "latitude" to location.latitude,
-                            "longitude" to location.longitude
-                        )
-                        docRef.set(locationData)
-                            .addOnSuccessListener {
-                                Log.d(TAG, "Localização enviada com sucesso!")
-                            }
-                            .addOnFailureListener {
-                                Log.e(TAG, "Erro ao enviar localização", it)
-                            }
-                    } else {
-                        Log.e(TAG, "Localização não encontrada")
-                    }
-                }
-                .addOnFailureListener { e ->
-                    Log.e(TAG, "Erro ao obter localização", e)
-                }
-        }
-        var panic_button: Button = findViewById(R.id.panic_button)
 
-        panic_button = findViewById(R.id.panic_button)
-        panic_button.setOnClickListener {
-            sendLocation()
-        }
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.RECORD_AUDIO
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            ActivityCompat.requestPermissions(this,
-                arrayOf(Manifest.permission.RECORD_AUDIO),
-                RECORD_AUDIO_PERMISSION_REQUEST_CODE)
-            return
-        }
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+            checkSelfPermission(Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+
+            // Permissões já concedidas
+            // TODO: Executar a lógica da atividade
+            screen.panicButton.setOnClickListener {
+                sendLocation()
+            }
+
             // Inicia o AudioCaptureThread
-            val audioCaptureService = AudioCaptureService()
+
             audioCaptureService.start()
             Log.d("AH_DEBUG", audioCaptureService.db.toString())
             Thread {
@@ -117,8 +83,62 @@ class MainActivity : AppCompatActivity() {
                     Thread.sleep(2000) // Espera 1 segundo antes de verificar novamente
                 }
             }.start()
+        }
+        else {
+            // Permissões ainda não concedidas
+            // Solicita as permissões
+            screen.tvSoundPressureLevel.text = "Libere as permissões nas configurações"
+            requestPermissions(PERMISSIONS, 0)
+        }
     }
 
+    fun sendLocation() {
+        // Obter a localização atual do dispositivo
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                if (location != null) {
+                    // Enviar a localização para o Firestore
+                    val db = FirebaseFirestore.getInstance()
+                    val docRef = db.collection("warnings").document()
+                    Log.d("TESTE_AH", location.latitude.toString())
+                    val locationData = hashMapOf(
+                        "latitude" to location.latitude,
+                        "longitude" to location.longitude,
+                        "db" to "${String.format("%.2f", audioCaptureService.db)}"
+                    )
+                    docRef.set(locationData)
+                        .addOnSuccessListener {
+                            Log.d(TAG, "Localização enviada com sucesso!")
+                        }
+                        .addOnFailureListener {
+                            Log.e(TAG, "Erro ao enviar localização", it)
+                        }
+                } else {
+                    Log.e(TAG, "Localização não encontrada")
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.e(TAG, "Erro ao obter localização", e)
+            }
+    }
     //Métodos são "fun"
     private fun createNotification() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -131,20 +151,30 @@ class MainActivity : AppCompatActivity() {
             manager.createNotificationChannel(channel)
         }
     }
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
+    // Método chamado quando o usuário concede ou nega as permissões solicitadas
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(grantResults[0] == 0) { //0 deu bom, -1 não aceitou
-            finish()
-            startActivity(intent)
-        } else {
-            screen.tvSoundPressureLevel.textSize = 20F
-            screen.tvSoundPressureLevel.text = "Libere a permissão de microfone nas configurações"
+        when (requestCode) {
+            0 -> {
+                // Verifica se as permissões foram concedidas
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    // Permissões concedidas
+                    // TODO: Executar a lógica da atividade
+                    // Reestarta a activity
+                    val intent = intent
+                    finish()
+                    startActivity(intent)
+                } else {
+                    // Permissões negadas
+                    // TODO: Tratar a negação das permissões
+                }
+                return
+            }
+            else -> {
+                // Outro pedido de permissão
+            }
         }
-
     }
 
     override fun onDestroy() {
